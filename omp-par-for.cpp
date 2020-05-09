11592edbce7c5b5119f6e99fbbf11099d0e1fc0d
maelso <maelso.bruno@dce.ufpb.br>
2020-05-09 14:33:33 -0300

    updating...

--- omp-par-for.cpp
+++ omp-par-for.cpp
@@ -14,27 +14,24 @@ int main(int argc, char const *argv[])
     int BORDER_SIZE = 0;
     int SPACE_ORDER = 0;
     int time_m = 0;
-    int time_M = 5;
-    int GRID_SIZE = 32;
+    int time_M = 10000;
+    int GRID_SIZE = 4096;
     int x_m = (int)BORDER_SIZE + SPACE_ORDER / 2;
     int x_M = (int)BORDER_SIZE + SPACE_ORDER / 2 + GRID_SIZE;
     int y_m = (int)BORDER_SIZE + SPACE_ORDER / 2;
     int y_M = (int)BORDER_SIZE + SPACE_ORDER / 2 + GRID_SIZE;
 
     int size_u[] = {GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER, GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER};
-    // float **u = (float **)malloc(TIME_ORDER * sizeof(float *));
-    // u[0] = (float *)calloc(size_u[0] * size_u[1], sizeof(float));
-    // u[1] = (float *)calloc(size_u[0] * size_u[1], sizeof(float));
     float *u = (float *)calloc(size_u[0] * size_u[1], sizeof(float));
 
     // print_array_2d(u, size_u[0], size_u[1]);
 
     // GPU working
     #pragma omp target enter data map(to \
-                                  : u [0:size_u[0] * (size_u[1] / 2)])
+                                  : u [0:size_u[0] * (size_u[1] / 2)]) nowait
     for (int time = time_m, t0 = (time) % (2), t1 = (time + 1) % (2); time < time_M; time += 1, t0 = (time) % (2), t1 = (time + 1) % (2))
     {
-    #pragma omp target teams distribute parallel for collapse(2)
+    #pragma omp target teams distribute parallel for collapse(2) nowait
         for (int x = x_m; x < x_M/2; x += 1)
         {
             for (int y = y_m; y < y_M; y += 1)
@@ -43,9 +40,6 @@ int main(int argc, char const *argv[])
             }
         }
     }
-    #pragma omp target update from(u [0:size_u[0] * (size_u[1] / 2)])
-    #pragma omp target exit data map(release \
-                                 : u [0:size_u[0] * (size_u[1] / 2)])
 
     // CPU working
     for (int time = time_m, t0 = (time) % (2), t1 = (time + 1) % (2); time < time_M; time += 1, t0 = (time) % (2), t1 = (time + 1) % (2))
@@ -59,8 +53,12 @@ int main(int argc, char const *argv[])
             }
         }
     }
+
+    #pragma omp target update from(u [0:size_u[0] * (size_u[1] / 2)])
+    #pragma omp target exit data map(release \
+                                 : u [0:size_u[0] * (size_u[1] / 2)])
     
-    print_array_2d(u, size_u[0], size_u[1]);
+    // print_array_2d(u, size_u[0], size_u[1]);
 
     free(u);
     printf("\nend.\n");
