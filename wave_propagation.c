876dcb20f70438c86bf52b1b942ffe017fa7a490
Maelso Pacheco <maelso.bruno@gmail.com>
2020-07-21 13:25:46 -0300

    2D wave propagation - Hybrid (CPU and GPU) implementation
    Equals time steps;
    Data transfer (halo transfer);
    The half of 1064 is not a multiple of 8.

--- wave_propagation.c
+++ wave_propagation.c
@@ -1,5 +1,7 @@
+#define _POSIX_C_SOURCE 200809L
+
 #include "stdio.h"
-#include <malloc.h>
+#include <stdlib.h>
 #include "omp.h"
 
 void print_array_2d(float *u, int x_size, int y_size);
@@ -8,42 +10,26 @@ void print_array_2d(float *u, int x_size, int y_size);
 int TIME_ORDER = 2;
 int DIMS = 2;
 
-int main(int argc, char const *argv[])
+
+int main()
 {
 
     int BORDER_SIZE = 0;
     int SPACE_ORDER = 2;
     int time_m = 1;
-    int time_M = 72;
-    int GRID_SIZE = 141;
+    int time_M = 894;
+    int GRID_SIZE = 1064;
     int x_m = (int)BORDER_SIZE + SPACE_ORDER;
     int x_M = (int)BORDER_SIZE + SPACE_ORDER + GRID_SIZE;
     int y_m = (int)BORDER_SIZE + SPACE_ORDER;
     int y_M = (int)BORDER_SIZE + SPACE_ORDER + GRID_SIZE;
 
-    int size_u[] = {GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER, GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER};
-    
-    float **vp;
-    float ***u;
+    const int size_u[] = {GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER, GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER};
 
-    // Allocate data
-    printf("Allocating u array with dimensions %dx%dx%d...\n", TIME_ORDER + 1, size_u[0], size_u[1]);
-    u = (float ***)malloc(sizeof(float **) * (TIME_ORDER + 1));
-    for (int i = 0; i < TIME_ORDER + 1; i++)
-    {
-        u[i] = (float **)malloc(sizeof(float *) * size_u[0]);
-        for (int j = 0; j < size_u[0]; j++)
-        {
-            u[i][j] = (float *)malloc(sizeof(float) * size_u[1]);
-        }
-    }
-
-    printf("Allocating vp array with dimensions %dx%d...\n", size_u[0], size_u[1]);
-    vp = (float **)malloc(sizeof(float *) * size_u[0]);
-    for (int j = 0; j < size_u[0]; j++)
-    {
-        vp[j] = (float *)malloc(sizeof(float) * size_u[1]);
-    }
+    float (*vp)[size_u[0]];
+    posix_memalign((void**)&vp, 64, sizeof(float[size_u[0]][size_u[1]]));
+    float (*u)[size_u[0]][size_u[1]];
+    posix_memalign((void**)&u, 64, sizeof(float[TIME_ORDER+1][size_u[0]][size_u[1]]));
 
     //inicializing values
     for (int j = 0; j < size_u[0]; j++)
@@ -57,59 +43,97 @@ int main(int argc, char const *argv[])
         }
     }
     // source injection
-    u[0][72][72] = 1.;
+    u[0][size_u[0]/2][size_u[1]/2] = 1.;
 
-    
-    //printing values
-    printf("Printing initial values...\n");
-    for (int j = 0; j < size_u[0]; j++)
-    {
-        for (int k = 0; k < size_u[1]; k++)
-        {
-            printf("%.3f ", u[0][j][k]);
-        }
-	printf("\n");
-    }
-    printf("\n\n");
-
-    int num_threads = 8;
+    int num_threads = 1;
 
     omp_set_num_threads(num_threads);
     #pragma omp parallel
     {
-        // CPU working
-        float r1 = 7.839999675750732421875;
+        #pragma omp master
+        {
+            #pragma omp target enter data map(to: vp[0:size_u[0]][0:size_u[1]])
+            #pragma omp target enter data map(to: u[0][0:size_u[0]][0:size_u[1]])
+            #pragma omp target enter data map(to: u[1][0:size_u[0]][0:size_u[1]])
+            #pragma omp target enter data map(to: u[2][0:size_u[0]][0:size_u[1]])
+        }
+
         for (int time = time_m, t0 = (time)%(3), t1 = (time + 1)%(3), t2 = (time + 2)%(3); time <= time_M; time += 1, t0 = (time)%(3), t1 = (time + 1)%(3), t2 = (time + 2)%(3))
         {
-            #pragma omp for schedule(guided)
-            for (int x = x_m - 1; x < x_M - 1; x += 1)
+            #pragma omp master
             {
-                for (int y = y_m - 1; y < y_M - 1; y += 1)
+                float r1 = 7.839999675750732421875;
+                // GPU working
+                // #pragma omp target enter data map(to: u[0][0:size_u[0]][0:size_u[1]])
+                #pragma omp target teams distribute parallel for collapse(2) nowait
+                for (int x = x_m - 1; x < (x_M - 1) ; x += 1)
                 {
-		        float r0 = vp[x + 2][y + 2] * vp[x + 2][y + 2];
-                u[t1][x + 2][y + 2] = -3.99999982e-2F * r0 * r1 * u[t0][x + 2][y + 2] + 
-                                       9.99999955e-3F * (r0 * r1 * u[t0][x + 1][y + 2] + 
-                                                         r0 * r1 * u[t0][x + 2][y + 1] + 
-                                                         r0 * r1 * u[t0][x + 2][y + 3] + 
-                                                         r0 * r1 * u[t0][x + 3][y + 2]) + 
-                                       1.99999991F * u[t0][x + 2][y + 2] - 
-                                       9.99999955e-1F * u[t2][x + 2][y + 2];
+                    for (int y = y_m - 1; y < y_M - 1; y += 1)
+                    {
+                    float r0 = vp[x + 2][y + 2] * vp[x + 2][y + 2];
+                    u[t1][x + 2][y + 2] = -3.99999982e-2F * r0 * r1 * u[t0][x + 2][y + 2] + 
+                                        9.99999955e-3F * (r0 * r1 * u[t0][x + 1][y + 2] + 
+                                                            r0 * r1 * u[t0][x + 2][y + 1] + 
+                                                            r0 * r1 * u[t0][x + 2][y + 3] + 
+                                                            r0 * r1 * u[t0][x + 3][y + 2]) + 
+                                        1.99999991F * u[t0][x + 2][y + 2] - 
+                                        9.99999955e-1F * u[t2][x + 2][y + 2];
+                    }
                 }
             }
+
+            // CPU working
+            // float r1 = 7.839999675750732421875;
+            // #pragma omp for schedule(guided)
+            // for (int x = (x_M - 1) / 2; x < x_M - 1; x += 1)
+            // {
+            //     for (int y = y_M - 1; y < y_M - 1; y += 1)
+            //     {
+            //     float r0 = vp[x + 2][y + 2] * vp[x + 2][y + 2];
+            //     u[t1][x + 2][y + 2] = -3.99999982e-2F * r0 * r1 * u[t0][x + 2][y + 2] + 
+            //                         9.99999955e-3F * (r0 * r1 * u[t0][x + 1][y + 2] + 
+            //                                             r0 * r1 * u[t0][x + 2][y + 1] + 
+            //                                             r0 * r1 * u[t0][x + 2][y + 3] + 
+            //                                             r0 * r1 * u[t0][x + 3][y + 2]) + 
+            //                         1.99999991F * u[t0][x + 2][y + 2] - 
+            //                         9.99999955e-1F * u[t2][x + 2][y + 2];
+            //     }
+            // }
+
+            // #pragma omp barrier
         }
-    }
-    
-    // print_array_2d(u, size_u[0], size_u[1]);
-    printf("\n********************************************\n");
-    for (int j = 0; j < size_u[0]; j++)
-    {
-	printf("%d \n", j);
-        for (int k = 0; k < size_u[1]; k++)
+        // #pragma omp target update from( (([3][size_u[0]][size_u[1]]) u) [t1][0:size_u[0]][0:size_u[1]])
+        #pragma omp master
         {
-            printf("%.8f ", u[1][j][k]);
+            #pragma omp target update from( u[0][0:size_u[0]][0:size_u[1]])
+            #pragma omp target update from( u[1][0:size_u[0]][0:size_u[1]])
+            #pragma omp target update from( u[2][0:size_u[0]][0:size_u[1]])
+
+            #pragma omp target exit data map(release: u[0][0:size_u[0]][0:size_u[1]])
+            #pragma omp target exit data map(release: u[1][0:size_u[0]][0:size_u[1]])
+            #pragma omp target exit data map(release: u[2][0:size_u[0]][0:size_u[1]])
+            #pragma omp target exit data map(release: vp[0:size_u[0]][0:size_u[1]])
         }
-        printf("\n");
     }
+
+    printf("***************************************************");
+    
+    FILE* arquivo = fopen("saida_07_07_20.txt", "w");
+    if(arquivo == NULL) {
+        fprintf(stderr, "Erro ao abrir o arquivo.txt.\n");
+        return 1;
+    }
+
+    // for(int i=0; i<TIME_ORDER+1; i++){
+        for(int j=0; j<size_u[0]; j++){
+            for(int k=0; k<size_u[1]; k++){
+                fprintf(arquivo, "%.10f ", u[0][j][k]);
+            }
+            fprintf(arquivo, "\n");
+        }
+    //     printf("\n\n");
+    // }
+    fclose(arquivo);
 
     free(u);
     free(vp);
