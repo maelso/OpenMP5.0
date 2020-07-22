21ce1fabe9f9235b57e0a7a88f173a5b7c7a5aaf
Maelso Pacheco <maelso.bruno@gmail.com>
2020-07-22 00:58:20 -0300

    2D Wave propagation working on GPU using 1D arrays

--- wave_propagation.c
+++ wave_propagation.c
@@ -7,7 +7,7 @@
 void print_array_2d(float *u, int x_size, int y_size);
 
 // Define global variables here
-int TIME_ORDER = 2;
+int TIME_ORDER = 3; // Change Request: Actually here we have time order 2, the value 3 is representing t0, t1 and t2
 int DIMS = 2;
 
 
@@ -17,6 +17,8 @@ int main()
     int BORDER_SIZE = 0;
     int SPACE_ORDER = 2;
     int time_m = 1;
+    // int time_M = 5;
+    // int GRID_SIZE = 8;
     int time_M = 894;
     int GRID_SIZE = 1064;
     int x_m = (int)BORDER_SIZE + SPACE_ORDER;
@@ -26,99 +28,107 @@ int main()
 
     const int size_u[] = {GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER, GRID_SIZE + 2 * BORDER_SIZE + 2 * SPACE_ORDER};
 
-    float (*vp)[size_u[0]];
-    posix_memalign((void**)&vp, 64, sizeof(float[size_u[0]][size_u[1]]));
-    float (*u)[size_u[0]][size_u[1]];
-    posix_memalign((void**)&u, 64, sizeof(float[TIME_ORDER+1][size_u[0]][size_u[1]]));
+    float *vp = (float *)calloc(size_u[0] * size_u[1], sizeof(float));
+    float *u = (float *)calloc(TIME_ORDER * size_u[0] * size_u[1], sizeof(float));
 
-    //inicializing values
-    for (int j = 0; j < size_u[0]; j++)
-    {
-        for (int k = 0; k < size_u[1]; k++)
-        {
-            u[0][j][k] = 0.0;
-            u[1][j][k] = 0.0;
-            u[2][j][k] = 0.0;
-	        vp[j][k] = 1.5;
+    int balance_factor = 1;
+    int time_offset = size_u[0] * size_u[1];
+    int gpu_data_domain = size_u[0] * size_u[1] / balance_factor;
+    
+    // time pointers
+    float *ut0 = u;
+    float *ut1 = u + time_offset;
+    float *ut2 = u + 2 * time_offset;
+    
+    // It will be used to change the time pointers
+    float *aux;
+
+    printf("initializing vp...\n");
+    //inicializing vp values
+    for (int j = 0; j < size_u[0]; j++){
+        for (int k = 0; k < size_u[1]; k++){
+            vp[j*size_u[0] + k] = 1.5;
         }
     }
+    printf("Injecting source...\n");
     // source injection
-    u[0][size_u[0]/2][size_u[1]/2] = 1.;
+    ut2[size_u[0] * (size_u[1]/2) + size_u[1]/2] = 1.;
+
+    // for(int j=0; j<size_u[0]; j++){
+    //     for(int k=0; k<size_u[1]; k++){
+    //         printf("%.10f ", ut0[j*size_u[0] + k] );
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n\n");
 
     int num_threads = 1;
+    printf("Using %d threads\n", num_threads);
 
     omp_set_num_threads(num_threads);
     #pragma omp parallel
     {
         #pragma omp master
         {
-            #pragma omp target enter data map(to: vp[0:size_u[0]][0:size_u[1]])
-            #pragma omp target enter data map(to: u[0][0:size_u[0]][0:size_u[1]])
-            #pragma omp target enter data map(to: u[1][0:size_u[0]][0:size_u[1]])
-            #pragma omp target enter data map(to: u[2][0:size_u[0]][0:size_u[1]])
+            #pragma omp target enter data map(to: vp[0:size_u[0] * size_u[1]])
+            printf("Sent vp to GPU!\n");
+            #pragma omp target enter data map(to: ut0[0:gpu_data_domain])
+            printf("Sent ut0 to GPU!\n");
+            #pragma omp target enter data map(to: ut1[0:gpu_data_domain])
+            printf("Sent ut1 to GPU!\n");
+            #pragma omp target enter data map(to: ut2[0:gpu_data_domain])
+            printf("Sent ut2 to GPU!\n");
         }
-
-        for (int time = time_m, t0 = (time)%(3), t1 = (time + 1)%(3), t2 = (time + 2)%(3); time <= time_M; time += 1, t0 = (time)%(3), t1 = (time + 1)%(3), t2 = (time + 2)%(3))
-        {
+        printf("Propagating...\n");
+        float r1 = 7.839999675750732421875;
+        for (int time = time_m; time <= time_M; time++){
             #pragma omp master
             {
-                float r1 = 7.839999675750732421875;
                 // GPU working
                 // #pragma omp target enter data map(to: u[0][0:size_u[0]][0:size_u[1]])
                 #pragma omp target teams distribute parallel for collapse(2) nowait
-                for (int x = x_m - 1; x < (x_M - 1) ; x += 1)
-                {
-                    for (int y = y_m - 1; y < y_M - 1; y += 1)
-                    {
-                    float r0 = vp[x + 2][y + 2] * vp[x + 2][y + 2];
-                    u[t1][x + 2][y + 2] = -3.99999982e-2F * r0 * r1 * u[t0][x + 2][y + 2] + 
-                                        9.99999955e-3F * (r0 * r1 * u[t0][x + 1][y + 2] + 
-                                                            r0 * r1 * u[t0][x + 2][y + 1] + 
-                                                            r0 * r1 * u[t0][x + 2][y + 3] + 
-                                                            r0 * r1 * u[t0][x + 3][y + 2]) + 
-                                        1.99999991F * u[t0][x + 2][y + 2] - 
-                                        9.99999955e-1F * u[t2][x + 2][y + 2];
+                for (int x = x_m; x <= x_M/balance_factor; x++){
+                    for (int y = y_m; y < y_M; y++){
+                        float r0 = vp[(x * size_u[0]) + y] * vp[(x * size_u[0]) + y];
+                        ut1[(x * size_u[0]) + y] =
+                            -3.99999982e-2F * r0 * r1 * ut0[(x * size_u[0]) + y] +
+                            9.99999955e-3F * (r0 * r1 * ut0[(x * size_u[0] - 1) + y] +
+                                              r0 * r1 * ut0[((x-1) * size_u[0]) + y] +
+                                              r0 * r1 * ut0[((x+1) * size_u[0]) + y] +
+                                              r0 * r1 * ut0[(x * size_u[0] + 1) + y]) +
+                            1.99999991F * ut0[(x * size_u[0]) + y] -
+                            9.99999955e-1F * ut2[(x * size_u[0]) + y];
                     }
                 }
+
+                #pragma omp target update from(ut0[0:gpu_data_domain])
+                #pragma omp target update from(ut1[0:gpu_data_domain])
+                #pragma omp target update from(ut2[0:gpu_data_domain])
+                aux = ut1;
+                ut1 = ut2;
+                ut2 = ut0;
+                ut0 = aux;
             }
-
-            // CPU working
-            // float r1 = 7.839999675750732421875;
-            // #pragma omp for schedule(guided)
-            // for (int x = (x_M - 1) / 2; x < x_M - 1; x += 1)
-            // {
-            //     for (int y = y_M - 1; y < y_M - 1; y += 1)
-            //     {
-            //     float r0 = vp[x + 2][y + 2] * vp[x + 2][y + 2];
-            //     u[t1][x + 2][y + 2] = -3.99999982e-2F * r0 * r1 * u[t0][x + 2][y + 2] + 
-            //                         9.99999955e-3F * (r0 * r1 * u[t0][x + 1][y + 2] + 
-            //                                             r0 * r1 * u[t0][x + 2][y + 1] + 
-            //                                             r0 * r1 * u[t0][x + 2][y + 3] + 
-            //                                             r0 * r1 * u[t0][x + 3][y + 2]) + 
-            //                         1.99999991F * u[t0][x + 2][y + 2] - 
-            //                         9.99999955e-1F * u[t2][x + 2][y + 2];
-            //     }
-            // }
-
-            // #pragma omp barrier
+            #pragma omp barrier
         }
         // #pragma omp target update from( (([3][size_u[0]][size_u[1]]) u) [t1][0:size_u[0]][0:size_u[1]])
         #pragma omp master
         {
-            #pragma omp target update from( u[0][0:size_u[0]][0:size_u[1]])
-            #pragma omp target update from( u[1][0:size_u[0]][0:size_u[1]])
-            #pragma omp target update from( u[2][0:size_u[0]][0:size_u[1]])
+            #pragma omp target update from( ut0[0:gpu_data_domain])
+            #pragma omp target update from( ut1[0:gpu_data_domain])
+            #pragma omp target update from( ut2[0:gpu_data_domain])
 
-            #pragma omp target exit data map(release: u[0][0:size_u[0]][0:size_u[1]])
-            #pragma omp target exit data map(release: u[1][0:size_u[0]][0:size_u[1]])
-            #pragma omp target exit data map(release: u[2][0:size_u[0]][0:size_u[1]])
-            #pragma omp target exit data map(release: vp[0:size_u[0]][0:size_u[1]])
+            #pragma omp target exit data map(release: vp[0:size_u[0] * size_u[1]])
+            #pragma omp target exit data map(release: ut0[0:gpu_data_domain])
+            #pragma omp target exit data map(release: ut1[0:gpu_data_domain])
+            #pragma omp target exit data map(release: ut2[0:gpu_data_domain])
         }
     }
 
-    printf("***************************************************");
+    printf("Saving data into .txt file\n");
+    printf("***************************************************\n");
     
-    FILE* arquivo = fopen("saida_07_07_20.txt", "w");
+    FILE* arquivo = fopen("saida_21_07_20_cpu_gpu.txt", "w");
     if(arquivo == NULL) {
         fprintf(stderr, "Erro ao abrir o arquivo.txt.\n");
         return 1;
@@ -127,13 +137,21 @@ int main()
     // for(int i=0; i<TIME_ORDER+1; i++){
         for(int j=0; j<size_u[0]; j++){
             for(int k=0; k<size_u[1]; k++){
-                fprintf(arquivo, "%.10f ", u[0][j][k]);
+                fprintf(arquivo, "%.10f ", ut0[j*size_u[0] + k] );
             }
             fprintf(arquivo, "\n");
         }
     //     printf("\n\n");
     // }
     fclose(arquivo);
+
+    // for(int j=0; j<size_u[0]; j++){
+    //     for(int k=0; k<size_u[1]; k++){
+    //         printf("%.10f ", ut0[j*size_u[0] + k] );
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n\n");
 
     free(u);
     free(vp);
